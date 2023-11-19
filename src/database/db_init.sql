e26d75577c776c72d53f5e9002178eaf1d37736b
jakub.sveda <jakub.sveda@icloud.com>
2023-11-19 23:02:23 +0100

    update db init tables - add leaders

--- src/database/db_init.sql
+++ src/database/db_init.sql
@@ -1,30 +1,52 @@
-create table users (
-    id bigserial primary key,
-    username varchar unique not null,
-    nickname varchar,
-    email varchar not null,
-    district_id integer references districts,
-    unit_id integer references units,
-    troop_id integer references troops,
-)
+create table districts
+(
+    id           bigserial primary key,
+    name         varchar not null,
+    address      varchar not null,
+    year_founded integer
+);
 
-create table districts (
-    id bigserial primary key,
-    name varchar not null,
-    address varchar not null,
+create table groups
+(
+    id           bigserial primary key,
+    name         varchar not null,
     year_founded integer,
-)
+    district_id  bigint references districts
+);
 
-create table units (
-    id bigserial primary key,
-    name varchar not null,
-    year_founded integer,
-    district_id integer references districts,
-)
+create table troops
+(
+    id          bigserial primary key,
+    name        varchar not null,
+    district_id bigint references districts,
+    group_id    bigint references groups
+);
 
-create table troops (
-    id bigserial primary key,
-    name varchar not null,
-    district_id integer references districts,
-    unit_id integer references units,
-)
+create table users
+(
+    id          bigserial primary key,
+    username    varchar not null unique,
+    nickname    varchar,
+    email       varchar not null,
+    district_id bigint references districts,
+    group_id    bigint references groups,
+    troop_id    bigint references troops
+);
+
+create table district_leaders
+(
+    district_id bigint not null unique references districts,
+    user_id     bigint not null references users
+);
+
+create table group_leaders
+(
+    group_id bigint not null unique references groups,
+    user_id  bigint not null references users
+);
+
+create table troop_leaders
+(
+    troop_id bigint not null unique references troops,
+    user_id  bigint not null references users
+);
