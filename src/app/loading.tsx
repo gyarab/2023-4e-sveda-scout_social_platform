1076e1504e30ca37aeba40ac1c10ed553e03a19d
jakub.sveda <jakub.sveda@icloud.com>
2024-03-11 20:08:46 +0100

    update loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,32 @@
+'use client'
+import {CircularProgress, Stack} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import theme from "@/components/ThemeRegistry/theme";
+
 export default function Loading() {
     return (
-        <div>
-            Loading...
-        </div>
+        <Stack
+            direction={'column'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            sx={{
+                height: '100vh'
+            }}
+        >
+            <CircularProgress
+                sx={{
+                    color: theme.palette.primary.main,
+                    fontSize: '100px'
+                }}
+            />
+            <Typography
+                sx={{
+                    color: theme.palette.primary.main,
+                    fontSize: '20px'
+                }}
+            >
+                Loading...
+            </Typography>
+        </Stack>
     )
 }
