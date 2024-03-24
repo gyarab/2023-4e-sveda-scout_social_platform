dd100ef3cf1d5090175baeb36bb89e3ce4767022
jakub.sveda <jakub.sveda@icloud.com>
2024-03-24 16:27:53 +0100

    clean project

--- src/database/db_init_data.sql
+++
@@ -1,2 +0,0 @@
-insert into users values (DEFAULT, 'jakub.sveda', null, 'jakub.sveda@icloud.com');
-insert into users values (DEFAULT, 'jakkk.sss', 'slim', 'jak.s@icloud.com');
--- src/database/testovanicko.ts
+++
@@ -1,7 +0,0 @@
-const uRef = {
-    current: {
-        value: ''
-    }
-}
-
-console.log(uRef.current.value)
